527e71bbae0f5379e8132b59793138bdf9632218
loadman <loadman@yandex.ru>
2023-07-04 20:23:40 +0300

    Creating an intermediary between data base and presentation layer + deleting unnecessary files

---
+++ MyHomeProject.DomainLayer/EstimatedGFRDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeProject.DomainLayer
+{
+    public class EstimatedGFRDTO
+    {
+        public decimal CreatinineBedsideSchwartz { get; set; }
+        public decimal CreatinineCystatinCCKiD { get; set; }
+        public decimal CystatinCBased { get; set; }
+        public decimal CreatinineSchwartzLyon { get; set; }
+        public decimal CreatininePottel { get; set; }
+        public decimal CreatinineFlandersMetadata { get; set; }
+    }
+}
---
+++ MyHomeProject.DomainLayer/PatientsAntropometricsDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeProject.DomainLayer
+{
+    public class PatientsAntropometricsDTO
+    {
+        public uint Id { get; set; }
+        public decimal Height { get; set; }
+        public decimal Age { get; set; }
+        public Gender Gender { get; set; }
+
+        public decimal BUN { get; set; }
+        public decimal SCreatinine { get; set; }
+        public decimal SCystatinC { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
