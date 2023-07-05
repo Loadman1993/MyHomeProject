324271034ef020ed7010b99e39c24cceaf2c6ce9
loadman <loadman@yandex.ru>
2023-07-06 02:21:12 +0300

    adding  intermediate class between calculations and DTO class file for subsequent interaction of DAL and Presentation layer

---
+++ MyHomeProject.DomainLayer/EstimatedGFRSetter.cs
@@ -0,0 +1,37 @@
+﻿using MyHomeProject.DomainLayer;
+using MyHomeProject.DomainLayer.Calculations;
+
+var estimatedGFR = new EstimatedGFRDTO();
+var setter = new EstimatedGFRSetter(estimatedGFR);
+var estimator = new CreatinineBedsideSchwartz();
+var antropometrics = new PatientsAntropometricsDTO
+{
+    
+    Id = 1,
+    Height = 180,
+    Age = 35,
+    SCreatinine = 1.2m
+};
+
+decimal result = estimator.Calculate(antropometrics);
+setter.SetCreatinineBedsideSchwartz(result);
+
+
+
+public class EstimatedGFRSetter
+{
+    private EstimatedGFRDTO estimatedGFR;
+
+    public EstimatedGFRSetter(EstimatedGFRDTO estimatedGFR)
+    {
+        this.estimatedGFR = estimatedGFR;
+    }
+
+    public void SetCreatinineBedsideSchwartz(decimal value)
+    {
+        estimatedGFR.CreatinineBedsideSchwartz = value;
+    }
+
+    
+}
+
